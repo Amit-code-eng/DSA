8de0fe105e1ba4d7028af228f4388a3bc9909a1d
Amit-code-eng <73630584+Amit-code-eng@users.noreply.github.com>
2023-06-27 20:21:18 +0530

    disjoint set union

---
+++ Imp questions/dsu.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>.h>
+using namespace std;
+
+class dsu_
+{
+
+public:
+    int n;
+    vector<int> p, r;
+
+    dsu_(int n)
+    {
+
+        this->n = n;
+
+        p.resize(n + 1);
+        r.resize(n + 1);
+
+        for (int i = 0; i < n; i++)
+        {
+
+            p[i] = i;
+            r[i] = 1;
+        }
+    }
+
+    int find(int x)
+    {
+
+        return p[x] == x ? x : x = find(p[x]);
+    }
+
+    bool union_(int a, int b)
+    {
+
+        a = find(a);
+        b = find(b);
+        if (a == b)
+            return 0;
+
+        if (r[a] > r[b])
+        {
+            p[b] = a;
+        }
+        else if (r[b] > r[a])
+        {
+
+            p[a] = b;
+        }
+        else
+        {
+
+            p[a] = b;
+            r[b]++;
+        }
+
+        return 1;
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
